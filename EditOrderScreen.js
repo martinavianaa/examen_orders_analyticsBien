9b166f55920be387b3a01f3977bc2a4b5e072a0e
martinavianaa <marviacam@alum.us.es>
2026-05-13 15:13:56 +0200

    Add files via upload

---
+++ EditOrderScreen.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from 'react'
+import { ScrollView, StyleSheet, View, Pressable } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import * as yup from 'yup'
+import { Formik } from 'formik'
+import { getById, update } from '../../api/OrderEndpoints'
+import InputItem from '../../components/InputItem'
+import TextRegular from '../../components/TextRegular'
+import TextError from '../../components/TextError'
+import * as GlobalStyles from '../../styles/GlobalStyles'
+import { showMessage } from 'react-native-flash-message'
+
+
+export default function EditOrderScreen ({ navigation, route }) {
+  const [backendErrors, setBackendErrors] = useState([])
+  const [order, setOrder] = useState({})
+  const [initialOrderValues, setInitialOrderValues] = useState({address: '', price: ''})
+  
+  const updateOrder = async (values) => {
+    setBackendErrors([])
+    try {
+      const updatedOrder = await update(order.id, values)
+      showMessage({
+        message: `Order ${updatedOrder.name} succesfully updated`,
+        type: 'success',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
+      })
+      navigation.navigate('OrdersScreen', { id: order.restaurantId })
+    } catch (error) {
+      console.log(error)
+      setBackendErrors(error.errors)
+    }
+  }
+
+  const validationSchema = yup.object().shape({
+    address: yup.string().max(255, 'Address is too long.').required('Address is not an optional field.'),
+    price: yup.string().max(255, 'Price is too long.').required('Price is not an optional field.')
+  })
+
+  useEffect(() => {
+    async function fetchOrderDetail () {
+      try {
+        const fetchedOrder = await getById(route.params.orderId)
+        setOrder(fetchedOrder)
+        setInitialOrderValues({
+          address: fetchedOrder.address,
+          price: fetchedOrder.price.toString()
+        })
+      } catch (error) {
+        showMessage({
+          message: `There was an error while retrieving order details (id ${route.params.id}). ${error}`,
+          type: 'error',
+          style: GlobalStyles.flashStyle,
+          titleStyle: GlobalStyles.flashTextStyle
+        })
+      }
+    }
+    fetchOrderDetail()
+  }, [route])
+
+
+
+  return (
+    <Formik
+      enableReinitialize
+      validationSchema={validationSchema}
+      initialValues={initialOrderValues}
+      onSubmit={updateOrder}>
+      {({ handleSubmit, setFieldValue, values }) => (
+        <ScrollView>
+          <View style={{ alignItems: 'center' }}>
+            <View style={{ width: '60%' }}>
+              <InputItem
+                name='address'
+                label='Address:'
+              />
+              <InputItem
+                name='price'
+                label='Price:'
+              />
+
+              <Pressable
+                onPress={ handleSubmit }
+                style={({ pressed }) => [
+                  {
+                    backgroundColor: pressed
+                      ? GlobalStyles.brandSuccessTap
+                      : GlobalStyles.brandSuccess
+                  },
+                  styles.button
+                ]}>
+                <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
+                  <MaterialCommunityIcons name='content-save' color={'white'} size={20}/>
+                  <TextRegular textStyle={styles.text}>
+                    Save
+                  </TextRegular>
+                </View>
+              </Pressable>
+            </View>
+          </View>
+        </ScrollView>
+      )}
+    </Formik>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 8,
+    height: 40,
+    padding: 10,
+    width: '100%',
+    marginTop: 20,
+    marginBottom: 20
+  },
+  text: {
+    fontSize: 16,
+    color: 'white',
+    textAlign: 'center',
+    marginLeft: 5
+  }
+})
---
+++ OrdersScreen.js
@@ -0,0 +1,318 @@
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, ScrollView, FlatList, ImageBackground, Image, Pressable } from 'react-native'
+import { showMessage } from 'react-native-flash-message'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { getDetail, getRestaurantAnalytics, getRestaurantOrders } from '../../api/RestaurantEndpoints'
+import { nextStatus } from '../../api/OrderEndpoints'
+
+import TextRegular from '../../components/TextRegular'
+import TextSemiBold from '../../components/TextSemibold'
+import * as GlobalStyles from '../../styles/GlobalStyles'
+import { API_BASE_URL } from '@env'
+import pendingOrderImage from '../../../assets/order_status_pending.png'
+import inProcessOrderImage from '../../../assets/order_status_in_process.png'
+import sentOrderImage from '../../../assets/order_status_sent.png'
+import deliveredOrderImage from '../../../assets/order_status_delivered.png'
+import ImageCard from '../../components/ImageCard'
+
+export default function OrdersScreen ({ navigation, route }) {
+  const [restaurant, setRestaurant] = useState({})
+  const [orders, setOrders] = useState([])
+  const [analytics, setAnalytics] = useState([])
+
+  useEffect(() => {
+    fetchRestaurantDetail()
+    fetchRestaurantOrders()
+  }, [route])
+
+  const fetchRestaurantAnalytics = async () => {
+    
+
+  }
+
+  const fetchRestaurantOrders = async () => {
+    try {
+      const fetchedOrders = await getRestaurantOrders(route.params.id)
+      setOrders(fetchedOrders)
+    } catch (error) {
+      showMessage({
+        message: `There was an error while retrieving orders. ${error} `,
+        type: 'error',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
+      })
+    }
+  }
+
+  const handleNextStatus = async (order) => {
+    try {
+      await nextStatus(order.id)
+      showMessage({
+        message: `Order ${order.id} advanced to the next status.`,
+        type: 'success',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
+      })
+      fetchRestaurantAnalytics()
+      fetchRestaurantOrders()
+    } catch (error) {
+      showMessage({
+        message: `Could not advance order ${order.id} to the next status. ${error}`,
+        type: 'error',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
+      })
+    }
+  }
+
+
+
+
+  const renderAnalytics = () => {
+    return (
+      <View style={styles.analyticsContainer}>
+          <View style={styles.analyticsRow}>
+            <View style={styles.analyticsCell}>
+              <TextRegular textStyle={styles.text}>
+                Invoiced today
+              </TextRegular>
+              <TextSemiBold textStyle={styles.text}>
+              TO DO
+              </TextSemiBold>
+            </View>
+            <View style={styles.analyticsCell}>
+              <TextRegular textStyle={styles.text}>
+                #Pending orders
+              </TextRegular>
+              <TextSemiBold textStyle={styles.text}>
+              TO DO
+              </TextSemiBold>
+            </View>
+          </View>
+
+          <View style={styles.analyticsRow}>
+            <View style={styles.analyticsCell}>
+                <TextRegular textStyle={styles.text}>
+                  #Delivered today
+                </TextRegular>
+                <TextSemiBold textStyle={styles.text}>
+                TO DO
+                </TextSemiBold>
+              </View>
+              <View style={styles.analyticsCell}>
+                <TextRegular textStyle={styles.text}>
+                  #Yesterday orders
+                </TextRegular>
+                <TextSemiBold textStyle={styles.text}>
+                  TO DO
+                </TextSemiBold>
+              </View>
+          </View>
+        </View>
+    )
+  }
+  const renderHeader = () => {
+    return (
+        <View>
+          <ImageBackground source={(restaurant?.heroImage) ? { uri: API_BASE_URL + '/' + restaurant.heroImage, cache: 'force-cache' } : undefined} style={styles.imageBackground}>
+            <View style={styles.restaurantHeaderContainer}>
+              <TextSemiBold textStyle={styles.textTitle}>{restaurant.name}</TextSemiBold>
+              <Image style={styles.image} source={restaurant.logo ? { uri: API_BASE_URL + '/' + restaurant.logo, cache: 'force-cache' } : undefined} />
+              <TextRegular textStyle={styles.description}>{restaurant.description}</TextRegular>
+              <TextRegular textStyle={styles.description}>{restaurant.restaurantCategory ? restaurant.restaurantCategory.name : ''}</TextRegular>
+            </View>
+          </ImageBackground>
+          {renderAnalytics()}
+        </View>
+    )
+  }
+
+  const getOrderImage = (status) => {
+    switch (status) {
+      case 'pending':
+        return pendingOrderImage
+      case 'in process':
+        return inProcessOrderImage
+      case 'sent':
+        return sentOrderImage
+      case 'delivered':
+        return deliveredOrderImage
+    }
+  }
+
+  const renderOrder = ({ item }) => {
+    
+    const statusImage = getOrderImage(item.status)
+    
+    return (
+      <ImageCard
+        imageUri={statusImage}
+        title={`Order created at ${item.createdAt}`}
+        onPress={() => {
+          console.log('Order pressed.')
+        }}
+      >
+        <TextRegular numberOfLines={2}>Status: {item.status}</TextRegular>
+        <TextRegular numberOfLines={2}>Address: {item.address}</TextRegular>
+        <TextSemiBold>{item.price.toFixed(2)}€</TextSemiBold>
+
+        <View style={styles.actionButtonsContainer}>
+          <Pressable
+            onPress={() => navigation.navigate('EditOrderScreen', { orderId: item.id })}
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed
+                  ? GlobalStyles.brandBlueTap
+                  : GlobalStyles.brandBlue
+              },
+              styles.actionButton
+            ]}>
+          <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
+            <MaterialCommunityIcons name='pencil' color={'white'} size={20}/>
+            <TextRegular textStyle={styles.text}>
+              Edit
+            </TextRegular>
+          </View>
+        </Pressable>
+
+        <Pressable
+            onPress={() => handleNextStatus(item)}
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed
+                  ? GlobalStyles.brandGreenTap
+                  : GlobalStyles.brandGreen
+              },
+              styles.actionButton
+            ]}>
+          <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
+            <MaterialCommunityIcons name='skip-next' color={'white'} size={20}/>
+            <TextRegular textStyle={styles.text}>
+              Advance
+            </TextRegular>
+          </View>
+        </Pressable>
+        </View>
+      </ImageCard>
+          )
+  }
+
+  const renderEmptyOrdersList = () => {
+    return (
+      <TextRegular textStyle={styles.emptyList}>
+        This restaurant has no orders yet.
+      </TextRegular>
+    )
+  }
+
+  const fetchRestaurantDetail = async () => {
+    try {
+      const fetchedRestaurant = await getDetail(route.params.id)
+      setRestaurant(fetchedRestaurant)
+    } catch (error) {
+      showMessage({
+        message: `There was an error while retrieving restaurant details (id ${route.params.id}). ${error}`,
+        type: 'error',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
+      })
+    }
+  }
+
+  return (
+      <>
+      <FlatList
+            style={styles.container}
+            data={orders}
+            renderItem={renderOrder}
+            keyExtractor={item => item.id.toString()}
+            ListHeaderComponent={renderHeader}
+            ListEmptyComponent={renderEmptyOrdersList}
+      />
+
+      </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  restaurantHeaderContainer: {
+    height: 250,
+    padding: 20,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  imageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
+  },
+  image: {
+    height: 100,
+    width: 100,
+    margin: 10
+  },
+  description: {
+    color: 'white'
+  },
+  textTitle: {
+    fontSize: 20,
+    color: 'white'
+  },
+  emptyList: {
+    textAlign: 'center',
+    padding: 50
+  },
+  text: {
+    fontSize: 16,
+    color: 'white',
+    alignSelf: 'center',
+    marginLeft: 5
+  },
+  actionButton: {
+    borderRadius: 8,
+    height: 40,
+    marginTop: 12,
+    margin: '1%',
+    padding: 10,
+    alignSelf: 'center',
+    flexDirection: 'column',
+    width: '50%'
+  },
+  actionButtonsContainer: {
+    flexDirection: 'row',
+    bottom: 5,
+    position: 'absolute',
+    width: '90%'
+  },
+  analyticsContainer: {
+    backgroundColor: GlobalStyles.brandPrimaryTap,
+    paddingVertical: 10
+  },
+  analyticsRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 5
+  },
+  analyticsCell: {
+    margin: 5,
+    color: 'white',
+    fontSize: 12,
+    width: '45%',
+    backgroundColor: GlobalStyles.brandPrimary,
+    borderRadius: 8,
+    paddingVertical: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.41,
+    shadowRadius: 3.11,
+    elevation: 2
+  }
+})
